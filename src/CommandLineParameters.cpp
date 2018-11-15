605d2a7a560d1fde2887a9e2d14eb7833c709627
Pavel Dovgalyuk <pavel.dovgaluk@gmail.com>
2018-11-15 15:15:54 +0300

    Remove obsolete function from CommandLineParameters

--- src/CommandLineParameters.cpp
+++ src/CommandLineParameters.cpp
@@ -1,10 +1,5 @@
 #include "CommandLineParameters.h"
 
-QString CommandLineParameters::getNextID()
-{
-    return "device-" + QString::number(id++);
-}
-
 LaunchMode CommandLineParameters::getLaunchMode()
 {
     return mode;
--- src/CommandLineParameters.h
+++ src/CommandLineParameters.h
@@ -7,10 +7,9 @@
 class CommandLineParameters
 {
 public:
-    CommandLineParameters(LaunchMode mode) : id(0), mode(mode), diskID(0) {}
-    CommandLineParameters() : id(0), mode(LaunchMode::NORMAL), diskID(0) {}
+    CommandLineParameters(LaunchMode mode) : mode(mode), diskID(0) {}
+    CommandLineParameters() : mode(LaunchMode::NORMAL), diskID(0) {}
     
-    QString getNextID();
     LaunchMode getLaunchMode();
 
     /* record|replay */
@@ -23,7 +22,6 @@ private:
     QString getNextOverlayName();
 
 private:
-    int id;
     LaunchMode mode; /* normal, record, replay */
     QStringList images;
     QStringList overlays;
