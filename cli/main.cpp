f59653da89463adb45a8560ea75012b58c35a4c3
Pavel Dovgalyuk <pavel.dovgaluk@gmail.com>
2020-03-18 10:02:42 +0300

    Added cmdline qemu-cli command

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,15 +1,18 @@
 #include <QTextStream>
 #include "GlobalConfig.h"
+#include "CommandLineParameters.h"
 
 QTextStream out(stdout);
 
 void usage()
 {
     out << "qemu-cli usage:\n"
-        "list - output configured VMs\n";
+        "list - output configured VMs\n"
+        "cmdline <vm>"
+        " - output command line for running specified VM\n";
 }
 
-void vmlist()
+int vmlist()
 {
     GlobalConfig cfg;
     foreach(VMConfig *vm, cfg.get_exist_vm())
@@ -17,6 +20,21 @@ void vmlist()
         out << "===============================\n";
         out << vm->get_vm_info();
     }
+    return 0;
+}
+
+int vmcmdline(const char *name)
+{
+    GlobalConfig cfg;
+    VMConfig *vm = cfg.get_vm_by_name(name);
+    if (!vm)
+    {
+        out << "VM " << name << " does not exist\n";
+        return 1;
+    }
+    CommandLineParameters params;
+    out << vm->getCommandLine(params) << "\n";
+    return 0;
 }
 
 int main(int argc, char *argv[])
@@ -29,11 +47,24 @@ int main(int argc, char *argv[])
 
     if (!strcmp(argv[1], "list"))
     {
-        vmlist();
+        return vmlist();
+    }
+    else if (!strcmp(argv[1], "cmdline"))
+    {
+        if (argc < 3)
+        {
+            usage();
+            return 1;
+        }
+        else
+        {
+            return vmcmdline(argv[2]);
+        }
     }
     else
     {
         usage();
+        return 1;
     }    
 
     return 0;
