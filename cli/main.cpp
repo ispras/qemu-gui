3cc001d16b615e65b99925169620de46ae5d35c1
Pavel Dovgalyuk <pavel.dovgaluk@gmail.com>
2020-03-18 09:38:07 +0300

    VM list function for qemu-cli

--- cli/main.cpp
+++ cli/main.cpp
@@ -1,12 +1,40 @@
+#include <QTextStream>
 #include "GlobalConfig.h"
 
-int main(int argc, char *argv[])
+QTextStream out(stdout);
+
+void usage()
+{
+    out << "qemu-cli usage:\n"
+        "list - output configured VMs\n";
+}
+
+void vmlist()
 {
     GlobalConfig cfg;
     foreach(VMConfig *vm, cfg.get_exist_vm())
     {
-        qInfo() << vm->get_vm_info();
+        out << "===============================\n";
+        out << vm->get_vm_info();
     }
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc < 2)
+    {
+        usage();
+        return 0;
+    }
+
+    if (!strcmp(argv[1], "list"))
+    {
+        vmlist();
+    }
+    else
+    {
+        usage();
+    }    
 
     return 0;
 }
